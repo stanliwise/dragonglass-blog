5eba0410b99c69e827a3bcea4f079d87eac963ad
mrphemi <mrlapinho@gmail.com>
2019-04-07 15:16:45 +0100

    Add nav.php to post page

--- hng/post.php
+++ hng/post.php
@@ -5,31 +5,8 @@
    <div class="parent padd">
       <div class="container">
          <div class="row">
-            <div class="col-sm-3">
-                  <div class="row">
-                     <div class="col-sm-9">
-                        <div class="row">
-                           <img class="img-responsive profile-pic" src="images/fiverr.jpg" alt="author picture">
-                           <div class="row text-center">
-                              <h3>TONI ATUNRASE</h3>
-                              <h4 class="occupation">Web/Software Developer</h4>
-                              <p>Software Enginner at AWS design enthusiast and music lover</p>
-                              <i class="fa fa-medium"></i>
-                              <i class="fa fa-github"></i>
-                              <i class="fa fa-twitter"></i>
-                           </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                           <ul class="side_nav">
-                           <li><a class="side_nav" href="#home">Home</a></li>
-                           <li><a class="side_nav" href="#news">News</a></li>
-                           <li><a class="side_nav" href="#contact">Contact</a></li>
-                           <li><a class="side_nav" href="#about">About</a></li>
-                           </ul>
-                        </div>
-                     </div>
-                  </div>
+               <div class="col-sm-3">
+                  <?php include'includes/nav.php'; ?>
                </div>
                <div class="col-sm-9 post-content">
                   <h2 class="blog-title">Who Owns Your Health Data</h2>
