0d9759abe23a0f8217d1a71e37717ebf0f8a5be7
DmatrixNg <49094365+DmatrixNg@users.noreply.github.com>
2019-04-07 10:59:12 +0100

    landing page, footer, header file created

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+include 'landing-page.php';
 
-   <a href="/Authentication/login.php">Login/Signup</a>
-
-</body>
-</html>
+ ?>
