41ad8c0149e00761162d761076cb195a4471b558
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-07 15:36:39 +0100

    Redirect to timeline after authentication

--- Authentication/index.php
+++ Authentication/index.php
@@ -1,14 +1,1 @@
-<?php header('location: /Authentication/auth/index.php'); //Not really clear on why there are two homepages :/ ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    REPLACE THIS WITH FE HOMEPAGE
-</body>
-</html>
+<?php header('location: /hng'); ?>
